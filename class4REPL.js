9f4f02d60a1371fec7272eeba0d4965d13d0c4a3
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-05-01 16:52:18 +0530

    files added

---
+++ class4REPL.js
@@ -0,0 +1,39 @@
+// REPL features of Node is very useful in experimenting with Nodejs code and to debug js code
+
+// R-READ - read user"s input ,parses the input into jsdata -structure
+// E-EVAL - takes and evaluated the data structures
+// P-PRINT - Prints and result
+// L-LOOP  - Loops the above command until the user presses ctrl-c twice.
+// type nul > class5.js   to create file from cmd
+
+
+// bellow examples are for REPL and need to run on terminal or cmd (to run cmd or terminal first type "node" to enter)
+// 1:JS Expression                                                                                      ----
+// > 3 + 3
+// above call as expression
+
+
+
+// 2: use variables
+// >var b = "askahy";
+// above is variable
+
+
+// 3: Multiline 
+
+
+// 4: use(_) to get the last result
+
+// 10+5
+// 15+5
+// _+5  result=> 25
+
+
+
+// 5: we can use editor mode
+ 
+
+// press TAB 2 times to get various features of nodeJs.
+
+
+
---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("welcome to node js");
+console.log("enter new file")
+
+// node index.js
+
+// echo "# node_samples" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/AkshayChavhan/node_samples.git
+// git push -u origin main
